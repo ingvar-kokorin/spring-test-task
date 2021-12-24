8b25e9756292674cd0b434efd0cfafb2611b92b8
Ingvar Kokorin <ingvar.kokorin@gmail.com>
2021-12-25 00:31:08 +0300

    Initial commit

---
+++ src/main/java/com/game/controller/PlayerController.java
@@ -0,0 +1,25 @@
+package com.game.controller;
+
+import com.game.entity.Player;
+import com.game.service.PlayerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+public class PlayerController {
+    private final PlayerService playerService;
+
+    @Autowired
+    public PlayerController(PlayerService playerService) {
+        this.playerService = playerService;
+    }
+
+    @GetMapping("/rest/players")
+    public List<Player> findAll() {
+        return playerService.findAll();
+    }
+}
---
+++ src/main/java/com/game/service/PlayerService.java
@@ -0,0 +1,27 @@
+package com.game.service;
+
+import com.game.entity.Player;
+import com.game.repository.PlayerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PlayerService {
+
+    private final PlayerRepository playerRepository;
+
+    @Autowired
+    public PlayerService(PlayerRepository playerRepository) {
+        this.playerRepository = playerRepository;
+    }
+
+    public Player findById(Long id) {
+        return playerRepository.findById(id).get();
+    }
+
+    public List<Player> findAll() {
+        return playerRepository.findAll();
+    }
+}
