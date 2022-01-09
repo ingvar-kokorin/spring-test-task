33f1ff9891398416afca8aefae300d6d828ea247
Ingvar Kokorin <ingvar.kokorin@gmail.com>
2022-01-09 17:54:16 +0300

    Delete import

--- src/main/java/com/game/service/PlayerServiceImpl.java
+++ src/main/java/com/game/service/PlayerServiceImpl.java
@@ -16,7 +16,6 @@ import org.springframework.stereotype.Service;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 public class PlayerServiceImpl implements PlayerService {
